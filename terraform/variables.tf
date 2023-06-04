0292a6360ad8c68dee823391c18ec58773ec330d
bartodes <bartojdes@gmail.com>
2023-06-04 14:14:16 -0300

    feat: added root variables  tf file

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "environment_root" {
+    description = "The name of the environment."
+    type = string
+}
+
+variable "rg_name_root" {
+    description = "The name of the resource group."
+    type = string
+}
